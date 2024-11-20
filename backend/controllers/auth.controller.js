74563f9fd55b47b90a942591764b502cf53dd9c6
shihab01118 <shahed.shihab.0@gmail.com>
2024-11-21 01:50:45 +0600

    verification email sending functionality added at sign up route using mailtrap

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,35 +2,46 @@ import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateVerificationToken } from "../utils/generateVerificationToken.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
 
   try {
+    // check if all the parameters are provided
     if (!email || !password || !name) {
       throw new Error("All fields are Required!");
     }
 
+    // check if the user is already exists
     const userAlreadyExists = await User.findOne({ email });
     if (userAlreadyExists) {
       return res.status(400).json();
     }
 
+    // send verification code to user's email
+    const verificationToken = generateVerificationToken();
+    await sendVerificationEmail(email, verificationToken);
+
+    // generate token and set in cookie
+    generateTokenAndSetCookie(res, email);
+
+    // convert password to hash encryption
     const hashedPassword = await bcryptjs.hash(password, 10);
 
+    // create new user based on the schema
     const user = new User({
       email,
       password: hashedPassword,
       name,
-      verificationToken: generateVerificationToken(),
+      verificationToken,
       verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
     });
 
+    // save user to database
     await user.save();
 
-    // jwt
-    generateTokenAndSetCookie(res, user._id);
-
+    // send response
     res.status(201).json({
       success: true,
       message: "User Created Successfully!",
---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,23 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify Your Email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+    console.log("Email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending verification email", error);
+    throw new Error("Error sending verification email");
+  }
+};
