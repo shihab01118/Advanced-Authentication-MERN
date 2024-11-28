bcc68f87f6c237c1feb264d7b73c41f9bf5c9616
shihab01118 <shahed.shihab.0@gmail.com>
2024-11-28 12:55:40 +0600

    environment is ready to deploy project

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -21,6 +23,14 @@ app.use(express.json()); // allows us to parse incoming requests: req.body
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(5000, async () => {
   await connectDB();
