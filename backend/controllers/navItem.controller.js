685c7d84bb066a10e9c5e00972852f83c43c557e
shihab01118 <shahed.shihab.0@gmail.com>
2024-11-29 21:20:18 +0600

    api added for getting all nav items and deleting nav item by id

--- backend/controllers/navItem.controller.js
+++ backend/controllers/navItem.controller.js
@@ -1,5 +1,16 @@
 import NavItem from '../models/navItem.model.js';
 
+// endpoint function to get all nav items
+export const getAllNavItems = async (req, res) => {
+  try {
+    const navItems = await NavItem.find().sort({ order: 1 });
+    res.status(200).json({ success: true, data: navItems });
+  } catch (error) {
+    console.log('Error in get all nav items', error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
 // endpoint function to create a nav item
 export const createNavItem = async (req, res) => {
   try {
@@ -10,12 +21,13 @@ export const createNavItem = async (req, res) => {
         .json({ success: false, message: 'Label and link are required!' });
     }
 
-    const itemAlreadyExists =
-      (await NavItem.findOne({ label })) || (await NavItem.findOne({ link }));
+    const itemAlreadyExists = await NavItem.findOne({
+      $or: [{ label }, { link }]
+    });
     if (itemAlreadyExists) {
       return res
         .status(409)
-        .json({ success: false, message: 'Label already exists!' });
+        .json({ success: false, message: 'Label or link already exists!' });
     }
 
     const newOrder = order ?? (await NavItem.countDocuments()) + 1;
@@ -33,5 +45,29 @@ export const createNavItem = async (req, res) => {
   } catch (error) {
     console.log('Error in create nav item: ', error);
     res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// endpoint function to delete a nav item
+export const deleteNavItem = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedItem = await NavItem.findByIdAndDelete(id);
+
+    if (!deletedItem) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Nav Item not found!' });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Item deleted successfully!',
+      data: deletedItem
+    });
+  } catch (error) {
+    console.log('Error deleting nav item: ', error);
+    res.status(500).json({ success: false, message: 'Internal server error!' });
   }
 };
--- backend/routes/navItem.route.js
+++ backend/routes/navItem.route.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { createNavItem } from '../controllers/navItem.controller.js';
+import {
+  createNavItem,
+  deleteNavItem,
+  getAllNavItems
+} from '../controllers/navItem.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
+router.get('/', getAllNavItems);
+
 router.post('/', verifyToken, createNavItem);
+
+router.delete('/:id', verifyToken, deleteNavItem);
 
 export default router;
